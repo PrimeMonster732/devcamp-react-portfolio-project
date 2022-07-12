56f602a40b1294610b90ffd7360044f3340a316f
PrimeMonster732 <primemonster732@gmail.com>
2022-07-12 08:58:09 -0600

    Second commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import moment from "moment";
 
+import PortfolioContainer from "./portfolio/portfolio-container";
+import NavigationContainer from "./navigation/navigation-container";
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>Wa Luigi Portfolio</h1>
-        <div>
-          {moment().format('MMMM Do YYYY, h:mm:ss a')}
-        </div>
+        <NavigationContainer />
+        <h1>WaLuigi Portfolio</h1>
+        <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
+        <PortfolioContainer />
       </div>
     );
   }
